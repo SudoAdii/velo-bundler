48b3f3fa51325c27acd5348f1b568b0c1a192144
Sudo Adi <141738671+SudoAdii@users.noreply.github.com>
2025-04-18 17:18:23 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      animation: {
+        float0: 'float 6s ease-in-out infinite',
+        float1: 'float 7s ease-in-out infinite',
+        float2: 'float 8s ease-in-out infinite',
+        float3: 'float 5s ease-in-out infinite',
+        float4: 'float 9s ease-in-out infinite',
+        float5: 'float 10s ease-in-out infinite',
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0px)' },
+          '50%': { transform: 'translateY(-20px)' },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
